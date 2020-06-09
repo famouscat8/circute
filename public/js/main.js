c8787902876daa1cfeafbfc47a4115a4689e6da4
root <3142362556@qq.com>
2020-06-09 17:11:30 +0800

    新增'签到'类型帖子

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,39 @@
 // index.html 的 js
 
-$(".btn-tools-wenku").click(()=>{});
-
-// 将标题临时保存在localStorage:spost_title
-$(".startfunny-start").click(()=>{
-    var title=$(".startfunny-title").val();
-    let localdb = new LocalDB();
-    localdb.setItem("spost_title",title).then(r=>{
-	layer.msg("标题已保存");
-	var url=location.href+"post.html";
-	// 新标签打开编辑帖子界面
-	window.open(url);
-    }).catch(e=>{
-	layer.msg("标题保存失败:" +e.e)
+$(()=>{
+    var ajax = new MyAjax();
+    // 将标题临时保存在localStorage:spost_title
+    $(".startfunny-start").click(()=>{
+	var title=$(".startfunny-title").val();
+	let localdb = new LocalDB();
+	localdb.setItem("spost_title",title).then(r=>{
+	    layer.msg("标题已保存");
+	    var url=location.href+"post.html";
+	    // 新标签打开编辑帖子界面
+	    window.open(url);
+	}).catch(e=>{
+	    layer.msg("标题保存失败:" +e.e)
+	});
     });
-});
 
-// 从服务器获取帖子
-$.ajax({
-    type:"POST",
-    contentType:"application/json;charset=UTF-8",
-    url:"/post",
-    data:JSON.stringify({page:1,pagesize:10,}),
-    success:re=>{
-	console.dir(re);
-	if(re.state=="1"){
-	    var vt   = new ViewTools();
-	    var tools= new Tools();
-	    var dom  = new Dom();
-	    vt.item1(re.posts,dom,tools,$);
-	    // 隐藏加载提示
-	    $(".loading").fadeOut();
-	}
-    },error:e=>{
-	console.log("main.js>post:/post: error");
+    // 从后端获取帖子成功后调用显示帖子数据
+    function init(posts){
+	var tools= new Tools();
+	var dom  = new Dom();
+	mMainList.init(posts,tools,$);
+    }
+    
+    function success(data){
+	console.dir(data);
+	if(data.state=="1")
+	    init(data.posts);
+	else console.log("main.js-->get post data error");
+    }
+    function error(e){
 	console.dir(e);
     }
-});
+
+    var post_data = {page:1,pagesize:10}
+    ajax.post("/post",JSON.stringify(post_data),success,error);
+})
 
